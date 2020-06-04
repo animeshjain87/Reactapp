e0ac74ab3f89514f449202161eb1a6ce86622864
My PC <b.surbhichoudhary@gmail.com>
2020-06-04 16:17:46 +0530

    Updates

---
+++ precache-manifest.0ce61631bab6480f8e4abed655d449cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1135e7f703e6bbdd371ffc39ed587dc3",
+    "url": "/Reactapp/index.html"
+  },
+  {
+    "revision": "01149988f75958989c65",
+    "url": "/Reactapp/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "f7b6bbd90743886d36a2",
+    "url": "/Reactapp/static/css/main.edec15fd.chunk.css"
+  },
+  {
+    "revision": "01149988f75958989c65",
+    "url": "/Reactapp/static/js/2.c7c8d54c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Reactapp/static/js/2.c7c8d54c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b6bbd90743886d36a2",
+    "url": "/Reactapp/static/js/main.f2cf9c7f.chunk.js"
+  },
+  {
+    "revision": "cabc7c9953e0fcc40c84",
+    "url": "/Reactapp/static/js/runtime-main.ce3a9552.js"
+  }
+]);
